f4073d39a61668c6506d7fa9c73e0a0f376a7bca
RxtBD <920164255@qq.com>
2020-03-23 17:23:01 +0800

    修改宽高不是4的倍数导致90127错误的bug；优化处理进度显示

--- mtcnn_asf_eval.py
+++ mtcnn_asf_eval.py
@@ -29,7 +29,6 @@ normalization = False    # 是否标准化，默认否
 
 data_path = "E:/testData/lfw"
 
-
 # 保存测试集人物信息：姓名，特征大小，特征，创建时间
 class userInfo():
     def __init__(self, username, feature_size, feature, create_time):
@@ -77,10 +76,10 @@ if __name__ == '__main__':
             if len(already_existing_list) % 100 == 0:
                 print("已处理 %.3f %%，耗时 %.3f s" % (len(already_existing_list) / len(set(names_list)) * 100.0,
                                                           time.time() - start))
-    print("数据预处理完成，耗时：", time.time() - start)
+    print("数据预处理完成，耗时 %.3f s" % (time.time() - start))
 
-    print("fileList:", emb_file_list)
-    print("nameList:", emb_name_list)
+    print("emb_file_list:", emb_file_list)
+    print("emb_name_list:", emb_name_list)
     print("already_existing_list:", already_existing_list)
 
     # 5.遍历数据集，构建特征集容器
@@ -101,38 +100,49 @@ if __name__ == '__main__':
     ASF_FaceFeature_List = []
     ASF_Name_List = []
     for r in userInfoList:
-        print(r[0], r[1], r[2], r[3])
-        ASF_Name_List.append(r[0])  # 标签
-        feat = r[2]  # feature字段，特征
+        print(r.username, r.feature_size, r.feature, r.create_time)
+
+        feat = r.feature  # feature字段，特征
         feat = feat[feat.index("'") + 1: -1]
         if len(feat) % 2 != 0:
             feat = feat + "="
         feat = base64.b64decode(feat)
         fas = face_class.ASF_FaceFeature()  # 特征
-        fas.featureSize = r[1]
+        fas.featureSize = r.feature_size
         fas.feature = face_dll.malloc(fas.featureSize)
         face_dll.memcpy(fas.feature, feat, fas.featureSize)
-        ASF_FaceFeature_List.append(fas)
+        ASF_FaceFeature_List.append(fas)   # 特征
+        ASF_Name_List.append(r.username)  # 标签
 
     # 7.遍历数据集，进行人脸识别评测，测评内容：mtcnn+asf
     total_check_sum = len(files_list)
     correct_nums = 0
     reco_result = []    # 识别结果
+    image_count = 0
     for file, name in zip(files_list, names_list):    # 文件路径，标签
-        frame = cv2.imdecode(np.fromfile(file, dtype=np.uint8), cv2.IMREAD_COLOR)
-        im = face_class.IM()
-        im.data = frame
-        im.width = frame.shape[1]
-        im.height = frame.shape[0]
+        # frame = cv2.imdecode(np.fromfile(file, dtype=np.uint8), cv2.IMREAD_COLOR)
+        # im = face_class.IM()
+        # frame = cv2.resize(frame, (frame.shape[1] // 4 * 4, frame.shape[0] // 4 * 4))    # 宽高要reshape成4的倍数，否则会报错：90127
+        # im.data = frame
+        # im.width = frame.shape[1]
+        # im.height = frame.shape[0]
+        image_count += 1    # 当前为第几个图片
+        if image_count % 100 == 0:
+            print("已完成：", image_count)
 
-        bboxes, landmarks = face_detect.detect_face(frame)
+        im = face_class.IM()
+        im.filepath = file
+        im = fun.LoadImg(im)  # 加载图片，这种方式会自动将宽高reshape成4的倍数
+
+        bboxes, landmarks = face_detect.detect_face(im.data)
         bboxes, landmarks = face_detect.get_square_bboxes(bboxes, landmarks, fixed="height")  # 以高为基准，获得等宽的矩形
         if bboxes == [] or landmarks == []:
-            print("-----no face")
+            # print("-----no face")
+            total_check_sum -= 1    # 如果没脸，则该图片不能作为验证集
+            reco_result.append((file, name, "No Face", "No Score"))
         else:
-            print("-----now have {} faces".format(len(bboxes)))
-
-            print("faces.faceNum:", len(bboxes))
+            # print("-----now have {} faces in {}".format(len(bboxes), im.filepath))
+            # print("faces.faceNum:", len(bboxes))
             for i in range(0, len(bboxes)):
                 # ra = faces.faceRect[i]
                 box = bboxes[i]
@@ -149,11 +159,13 @@ if __name__ == '__main__':
                 ret, fea = fun.Feature_extract(im, ft)  # 返回tuple，(标识, 特征)
                 if ret == 0:  # 特征提取成功
                     pred_name, pred_score = fun.asf_compare_embadding(fea, ASF_FaceFeature_List, ASF_Name_List)  # 识别标签，分数
+                    # print(pred_name, pred_score)
                     if pred_name == name:    # 如果识别姓名和标记姓名相同，则认为识别正确
                         correct_nums += 1
                     reco_result.append((file, name, pred_name, pred_score))    # 文件名，标注名，识别名，置信度
                 else:  # 同一帧图片有多个人脸的情况，“特征提取失败”会打印多次
-                    print("特征提取失败！")
+                    print("特征提取失败：", file)
+                    reco_result.append((file, name, "Feature Failed", "No Score"))
 
     # 8.测评完成
     print("EVAL finished! Accuracy: %.3f %%" % (correct_nums / total_check_sum * 100.0))
